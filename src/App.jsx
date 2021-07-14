ea892fcab73f468adecdf5c19f8462a34f2812e8
Pedro Arthur <pedro.veloso@spot-edu.com.br>
2021-07-14 17:14:03 -0300

    First commit, created NiceTranslator component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NiceTranslator from './components/NiceTranslator';
+
+function App() {
+  return (
+    <div className="App">
+      <NiceTranslator />
+    </div>
+  );
+}
+
+export default App;
