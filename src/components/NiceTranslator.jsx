286901c9179e625d470a09789fd6cf9d416a4d67
Pedro Arthur <pedro.veloso@spot-edu.com.br>
2021-07-14 18:13:09 -0300

    Info tweaks & textarea state testing

--- src/components/NiceTranslator.jsx
+++ src/components/NiceTranslator.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 
 const NiceTranslator = () => {
-  const [counter, setCounter] = useState(0);
+  const [englishText, setEnglishText] = useState('');
+
+  const handleTextChange = (event) => {
+    setEnglishText(event.target.value);
+  };
 
   return (
     <div className="translator-container">
-      <textarea className="translator-container__textarea"></textarea>
-      <button onClick={() => setCounter(counter + 1)}>Add to Counter</button>
-      <p>{counter}</p>
+      <h1>The Nice Translator App</h1>
+      <textarea
+        className="translator-container__textarea"
+        placeholder="Type your text in english right here..."
+        value={englishText}
+        onChange={handleTextChange}></textarea>
+      <button onClick={() => console.log(englishText)}>Print Text</button>
+      <p>{englishText}</p>
     </div>
   );
 };
