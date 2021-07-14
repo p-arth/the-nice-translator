78f7f201a6f226b5b8c64e9606283e9eecb02d6c
Pedro Arthur <pedro.veloso@spot-edu.com.br>
2021-07-14 17:19:26 -0300

    Simply testing NiceTranslator rendering

--- src/components/NiceTranslator.jsx
+++ src/components/NiceTranslator.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 
 const NiceTranslator = () => {
-  return null;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>Add to Counter</button>
+      <br></br>
+      <p>{counter}</p>
+    </div>
+  );
 };
 
 export default NiceTranslator;
