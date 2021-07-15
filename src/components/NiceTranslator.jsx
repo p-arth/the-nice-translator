b14e29e7f36c7a7fa390023f3d58849b3d9d0699
Pedro Arthur <pedro.veloso@spot-edu.com.br>
2021-07-14 21:57:19 -0300

    Added API calls

--- src/components/NiceTranslator.jsx
+++ src/components/NiceTranslator.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 const NiceTranslator = () => {
   const [englishText, setEnglishText] = useState('');
   const [languageOption, setLanguageOption] = useState('');
+  const [availableLanguages, setAvailableLanguages] = useState([]);
+  const [translatedText, setTranslatedText] = useState('');
 
   const handleTextChange = (event) => {
     setEnglishText(event.target.value);
@@ -13,10 +15,38 @@ const NiceTranslator = () => {
     setLanguageOption(event.target.value);
   };
 
+  // The ideal would be to optimize and use a dispatch, taking the axios request from Duck with all the fetching actions, but I kept it simple
   const handleTranslateClick = () => {
-    console.log(englishText);
-    console.log(languageOption);
+    return axios
+      .get(
+        `https://api-free.deepl.com/v2/translate?auth_key=${process.env.REACT_APP_DEEPL_KEY}&text=${englishText}&source_lang=EN&target_lang=${languageOption}`
+      )
+      .then((response) => {
+        let translation = response.data.translations[0].text;
+        setTranslatedText(translation);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
+
+  // The ideal would be to optimize and use a dispatch, taking the axios request from a Duck with all the fetching actions, but I kept it simple
+  const getLanguageOptions = () => {
+    return axios
+      .get(
+        `https://api-free.deepl.com/v2/languages?auth_key=${process.env.REACT_APP_DEEPL_KEY}&text=Hello, world&type=target`
+      )
+      .then((response) => {
+        setAvailableLanguages(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getLanguageOptions();
+  }, []);
 
   return (
     <div className="translator-container">
@@ -26,16 +56,24 @@ const NiceTranslator = () => {
         placeholder="Type your text in english right here..."
         value={englishText}
         onChange={handleTextChange}></textarea>
+
       <select className="translator-container__select" value={languageOption} onChange={handleOptionChange}>
-        <option value="german">German</option>
-        <option value="portuguese">Portuguese</option>
-        <option value="dutch">Dutch</option>
+        {availableLanguages.map((available) => {
+          return (
+            <option value={available.language} key={available.language}>
+              {available.name}
+            </option>
+          );
+        })}
       </select>
+
       <button className="translator-container__button" onClick={() => handleTranslateClick()}>
         Translate
       </button>
       <h3>Translated text:</h3>
-      <p>{englishText ? englishText : 'No text translated yet'}</p>
+      <div className="translator-container__box">
+        <p>{translatedText ? translatedText : 'No text was translated yet'}</p>
+      </div>
     </div>
   );
 };
